ef32e93adeb2a93ea850780fb1c54b6c6cc5933a
sandeep butte <sandeep030690butte@gmail.com>
2019-06-01 21:48:47 +0530

    Handling Next and Prev Month Buttons

--- src/components/Calendar.js
+++ src/components/Calendar.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import {getMonth, getDay} from "../helpers/helpers";
+import {getMonth, getDay, mapDateDay} from "../helpers/helpers";
 import "./common.scss";
 
 export class Calendar extends Component {
@@ -8,10 +8,78 @@ export class Calendar extends Component {
             this.state = {
                 currentDate: new Date()
             }
+            this.date = 1;
+    }
+    getDates = (firstDayMonth, NoOfDays) => {
+        debugger;
+        const items = [];
+        for (let i=0; i<6; i++){
+            items.push(
+                <tr>
+                    {this.getDateColoumn(i, firstDayMonth, NoOfDays)}
+                </tr>
+            )
+        }
+        return items;
+    }
+    getDateColoumn = (i, firstDayMonth, NoOfDays) => {
+        debugger;
+        let today = new Date();
+        let days = [];
+        for (let j=0; j<7; j++){
+            if (i === 0 && j<firstDayMonth){
+                days.push(<td></td>)
+            } else if (this.date > NoOfDays){
+                break;
+            } else{
+                if (this.date === today.getDate()){
+                    days.push(<td className="today_date">{this.date}</td>)
+                    this.date++;
+                }else {
+                    days.push(<td>{this.date}</td>);
+                    this.date++;
+                }
+            }
+        }
+        return days;
+    }
+    handleMonth = (action) => {
+        let currentDate = this.state.currentDate;
+        let selectedDate = {};
+        if (action.toLowerCase() === "prev"){
+            console.log("Previous Month");
+            if (currentDate.getMonth() === 0){
+                selectedDate.month = 11;
+                selectedDate.year = currentDate.getFullYear() - 1;
+            } else {
+                selectedDate.month = currentDate.getMonth() -1;
+                selectedDate.year = currentDate.getFullYear();
+            }
+            console.log(selectedDate);
+        } else {
+            console.log("next month");
+            if (currentDate.getMonth() === 11){
+                selectedDate.month = 0;
+                selectedDate.year = currentDate.getFullYear() +1 ;
+            } else {
+                selectedDate.month = currentDate.getMonth() + 1;
+                selectedDate.year = currentDate.getFullYear();
+            }
+        }
+        console.log(selectedDate);
+        let _selectedMonth = selectedDate.month;
+        let _selectedYear = selectedDate.year;
+        this.date = 1;
+        this.setState({
+            currentDate: new Date(_selectedYear, _selectedMonth, 1)
+        })
     }
     renderCalendar = () => {
         const {currentDate} = this.state;
+        debugger;
         let NoOfDays = this.getDaysInaMonth(currentDate.getFullYear(), currentDate.getMonth()+1);
+        let firstDayMonth = mapDateDay(currentDate.getFullYear(), currentDate.getMonth(), 1);
+        console.log(firstDayMonth);
         console.log(NoOfDays);
         let month = getMonth(currentDate.getMonth()+1);
         let MonthArray = [];
@@ -22,28 +90,23 @@ export class Calendar extends Component {
         return (
             <div className="Calendar">
                 <div className={"Month_year_Details_container"}>
-                    <button>PrevMonth</button>
+                    <button onClick={() => this.handleMonth("Prev")}>PrevMonth</button>
                     <h2>{month} {currentDate.getFullYear()}</h2>
-                    <button>PrevMonth</button>
+                    <button onClick={() => this.handleMonth("Next")}>NextMonth</button>
                 </div>
-                {/* <div className="week_days">
-                    <ul>
-                        <li></li>
-                    </ul>
-                </div> */}
                 <ul className="days_container">
                     <table>
                         <thead>
-                            <th></th>
+                            <th>Sun</th>
+                            <th>Mon</th>
+                            <th>Tue</th>
+                            <th>Wed</th>
+                            <th>Thr</th>
+                            <th>Fri</th>
+                            <th>Sat</th>
                         </thead>
+                        {this.getDates(firstDayMonth, NoOfDays)}
                     </table>
-                    {
-                        MonthArray.map((value, index) => {
-                            return (
-                                <li className="day">{value}</li>
-                            )
-                        })
-                    }
                 </ul>
             </div>
         )
--- src/helpers/helpers.js
+++ src/helpers/helpers.js
@@ -15,8 +15,10 @@ export const getMonth = (month) => {
     }
 }
 
-export const getDay = (date) => {
-    const dateDay = new Date(date);
+export const getDay = (year, month, day) => {
+    debugger
+    // console.log(new Date(date))
+    const dateDay = new Date(year, month, day).getDay();
     switch(dateDay){
         case 0: return "Sunday"
         case 1: return "Monday"
@@ -25,5 +27,20 @@ export const getDay = (date) => {
         case 4: return "Thrusday"
         case 5: return "Friday"
         case 6: return "Saturday"
+    }
+}
+
+export const mapDateDay = (year, month, day) => {
+    // debugger
+    // console.log(new Date(date))
+    const dateDay = new Date(year, month, day).getDay();
+    switch(dateDay){
+        case 0: return 0
+        case 1: return 1
+        case 2: return 2
+        case 3: return 3
+        case 4: return 4
+        case 5: return 5
+        case 6: return 6
     }
 }
